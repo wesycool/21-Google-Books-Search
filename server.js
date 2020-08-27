4753dd155402a4b0ad6cd4a1be138459eb8f3c4f
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-08-27 10:58:08 -0400

    Initial Files

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const apiRouter = require('./app/router')
+const app = express()
+const mongoose = require( 'mongoose' );
+
+
+const PORT = process.env.PORT || 8080
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebook" , 
+    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+ // for serving all the normal html
+app.use( express.static('client/build') )
+// for parsing incoming POST data
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+// for routes
+apiRouter(app)
+
+app.get('/*', function (req, res) {
+    console.log( `[/*] (${req.protocol}//${req.get('host')}/${req.originalUrl} -- sending file: ${__dirname}/client/build/index.html` );
+    res.sendFile(`${__dirname}/client/build/index.html`);
+ });
+ 
+
+app.listen(PORT, function() {
+    console.log( `Serving app at: http://localhost:${PORT}` )
+})
